50dd721311669ca2074fa44d126725a38a77c09f
Shweta <nayakshweta07@gmail.com>
2017-03-19 22:16:52 +0530

    Adding programs 001 to 016

---
+++ 005.py
@@ -0,0 +1,20 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74.0 # inches
+my_height_in_cms = round (my_height * 2.54)
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %r." % my_name
+print "He's %d inches tall." % my_height
+print "That is almost %f in centimeters." % my_height_in_cms
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d and %d I get %d." % (
+    my_age, my_height, my_weight, my_age + my_height + my_weight)
---
+++ 011.py
@@ -0,0 +1,21 @@
+print "How old are you?",
+age = raw_input('--> ')
+#print "How tall are you?",
+#height = raw_input()
+#print "How much do you weigh?",
+#weight = raw_input()
+
+#print "So, you're %r old, %r tall and %r heavy." % (
+#    age, height, weight)
+# so that is one instance where %r is useful.. when you cannot predict the format
+
+print "Give me a number",
+x = float(raw_input())
+print "Give me another one",
+y = float(raw_input())
+print "Here's a sum of the two",
+print x + y
+
+
+
+
